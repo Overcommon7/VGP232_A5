8f9a4433d393d0ac5866e73ec75f2cd942782512
Overcommon7 <natecapitalization@gmail.com>
2022-12-01 20:21:30 -0800

    Pushed Eveything To Main And Added Tests

---
+++ Assignment5/Assignment5/CharacterTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Assignment5
+{
+    [TestFixture]
+    public class CharacterTest
+    {
+        Character character;
+        [SetUp]
+        public void SetUp()
+        {
+            character = new Character("TestCharacter", RaceCategory.Human, 10);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            character = null;
+        }
+
+        [Test]
+        public void Character_Test_Damange()
+        {
+            int damage = 5;
+            int remainingHealth = character.Health - damage;
+            character.TakeDamage(damage);
+            Assert.AreEqual(character.Health, remainingHealth);
+        }
+
+        [Test]
+        public void Character_Test_Death()
+        {
+            character.Health = character.MaxHealth;
+            character.IsAlive = true;
+
+            character.RestoreHealth(character.Health);
+            Assert.AreEqual(character.IsAlive, false);
+        }
+
+        [Test]
+        public void Character_Test_RestoreHealth()
+        {
+            character.Health = (int)(character.MaxHealth * 0.5);
+            int totalHealth = character.Health + (int)(character.Health * 0.25);
+            character.RestoreHealth((int)(character.Health * 0.25));
+            Assert.AreEqual(character.Health, totalHealth);
+        }
+
+        public void Character_Test_Revive()
+        {
+            character.Health = 0;
+            character.IsAlive = false;
+
+            character.RestoreHealth(character.MaxHealth);
+            Assert.AreEqual(character.IsAlive, true);
+        }
+    }
+}
---
+++ Assignment5/Assignment5/InventoryTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Assignment5
+{
+    [TestFixture]
+    public class InventoryTest
+    {
+        Inventory inventory;
+        static Item axe = new Item("Axe", 1, ItemGroup.Equipment);
+        static Item sword = new Item("Sword", 1, ItemGroup.Equipment);
+        static Item HealthPotion = new Item("Health Potion", 1, ItemGroup.Consumable);
+
+        [SetUp]
+        public void SetUp()
+        {
+            inventory = new Inventory(10);
+            inventory.AddItem(axe);
+            inventory.AddItem(sword);
+            inventory.AddItem(HealthPotion);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            inventory = null;
+        }
+
+        [Test]
+        public void Test_Inventory_RemoveItem_Found()
+        {
+            Item item;
+            int originalSlots = inventory.AvailableSlots;
+            inventory.TakeItem(axe.Name, out item);
+            Assert.AreEqual(axe, item);
+            Assert.Less(originalSlots, inventory.AvailableSlots);
+        }
+
+        [Test]
+        public void Test_Inventory_RemoveItem_NotFound()
+        {
+            Item item;
+            int originalSlots = inventory.AvailableSlots;
+            inventory.TakeItem("Invalid", out item);
+            Assert.AreEqual(originalSlots, inventory.AvailableSlots);
+        }
+
+        [Test]
+        public void Test_Inventory_AddItem()
+        {
+            int originalSlots = inventory.AvailableSlots;
+            var key = new Item("Golden Door Key", 1, ItemGroup.Key);
+            inventory.AddItem(key);
+            Assert.Less(inventory.AvailableSlots, originalSlots);
+            Assert.AreEqual(inventory.ListAllItems().Contains(key), true);
+        }
+
+        [Test]
+        public void Test_Reset_Inventory()
+        {
+            inventory.Reset();
+            Assert.AreEqual(inventory.AvailableSlots, inventory.MaxSlots);
+            Assert.AreEqual(inventory.ListAllItems().Count, 0);
+        }
+    }   
+}
